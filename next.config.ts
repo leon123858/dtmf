38d817ef0f3f0e7b590eca840c8e54f8499862ff
leon123858 <a0970785699@gmail.com>
2025-06-14 14:40:45 +0800

    set backend IP

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
 	/* config options here */
 	output: 'standalone',
+
+	publicRuntimeConfig: {
+		apiIP: process.env.NEXT_PUBLIC_API_IP,
+	},
 };
 
 export default nextConfig;
--- src/app/lib/tripApi/config.ts
+++ src/app/lib/tripApi/config.ts
@@ -1,2 +1,11 @@
-export const baseURL = 'http://127.0.0.1:8080/query';
-export const wsURL = 'ws://127.0.0.1:8080/subscription';
+import getConfig from 'next/config';
+
+const { publicRuntimeConfig } = getConfig();
+
+// should set environment variable NEXT_PUBLIC_API_IP to the IP address of the API server
+export const baseURL = publicRuntimeConfig.apiIP
+	? `http://${publicRuntimeConfig.apiIP}/query`
+	: 'http://127.0.0.1:8080/query';
+export const wsURL = publicRuntimeConfig.apiIP
+	? `ws://${publicRuntimeConfig.apiIP}/subscription`
+	: 'ws://127.0.0.1:8080/subscription';
